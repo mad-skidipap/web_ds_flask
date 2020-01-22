a4e2c328656af9ed20e0785b4286c6b066cc83ed
rahmatfaisal <rahmat.faisal@sci.ui.ac.id>
2020-01-22 15:37:30 +0700

    make it better

---
+++ models.py
@@ -0,0 +1,18 @@
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.datasets import load_iris 
+from sklearn.model_selection import train_test_split
+
+import joblib 
+
+data = load_iris()
+
+x = data.data
+y = data.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+model = AdaBoostClassifier()
+model.fit(x_train, y_train)
+
+joblib.dump(model, 'model.dmp')
+
